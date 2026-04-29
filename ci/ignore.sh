78c65fc7036017514441837d7cf7ef31ab0cdfb8
Marian Csontos <mcsontos@redhat.com>
2026-04-29 14:23:43 +0200

    CI: Add scripts and data for skip/ignore
    In some cases tests were causing kernel panics or disk exhaustion.
    
    Some tests are not stable, some are not running correctly in gating
    environment, and some are broken by updates of packages in compose.

---
+++ ci/ignore.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Usage:
+# - Run tests:
+# # make check
+# - Filter failures:
+# # ci/ignore.sh < test/results/list
+
+IGNORE_FILE="$(dirname "$0")/IGNORE"
+if [[ ! -f "$IGNORE_FILE" ]]; then
+	echo "ERROR: Skip file '$IGNORE_FILE' not found" >&2
+	exit 2
+fi
+
+# Find all failed tests, sort the list, and filter out anything matching patterns in IGNORE_FILE:
+# sed: ignore all comments and print non empty lines only
+grep failed | sort | grep -v -f <(sed -n -e 's/\s*#.*$//' -e '/^.\+$/p' "$IGNORE_FILE)
---
+++ ci/tests.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Usage:
+# # ci/tests.sh . < test/results/list
+
+SKIP_FILE="$(dirname "$0")/SKIP"
+if [[ ! -f "$SKIP_FILE" ]]; then
+	echo "ERROR: Skip file '$SKIP_FILE' not found" >&2
+	exit 2
+fi
+
+TEST_DIR="${1:-"."}/test"
+if [[ ! -d "$TEST_DIR" ]]; then
+	echo "ERROR: Test directory '$TEST_DIR' not found" >&2
+	exit 2
+fi
+
+# Find all tests, skip ones matching pattern in SKIP_FILE and sort the result:
+# sed: ignore all comments and print non empty lines only
+(cd "$TEST_DIR" && find . -path '*.sh' | grep -v '\(/lib/\)') | sed 's|^\./||' | grep -v -f <(sed -n -e 's/\s*#.*$//' -e '/^.\+$/p' "$SKIP_FILE") | sort
