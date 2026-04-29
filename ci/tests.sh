fdf073124d9ef7951d8e29a0b86f4e19ad5a9214
Marian Csontos <mcsontos@redhat.com>
2026-04-29 14:55:23 +0200

    CI: Add filter which tests to run

--- ci/tests.sh
+++ ci/tests.sh
@@ -3,6 +3,12 @@
 # Usage:
 # # ci/tests.sh . < test/results/list
 
+FILTER_FILE="$(dirname "$0")/ONLY"
+if [[ ! -f "$FILTER_FILE" ]]; then
+	echo "ERROR: Filter file '$FILTER_FILE' not found" >&2
+	exit 2
+fi
+
 SKIP_FILE="$(dirname "$0")/SKIP"
 if [[ ! -f "$SKIP_FILE" ]]; then
 	echo "ERROR: Skip file '$SKIP_FILE' not found" >&2
@@ -17,4 +23,4 @@ fi
 
 # Find all tests, skip ones matching pattern in SKIP_FILE and sort the result:
 # sed: ignore all comments and print non empty lines only
-(cd "$TEST_DIR" && find . -path '*.sh' | grep -v '\(/lib/\)') | sed 's|^\./||' | grep -v -f <(sed -n -e 's/\s*#.*$//' -e '/^.\+$/p' "$SKIP_FILE") | sort
+(cd "$TEST_DIR" && find . -path '*.sh' | grep -v '\(/lib/\)') | sed 's|^\./||' | grep -f <(sed -n -e 's/\s*#.*$//' -e '/^.\+$/p' "$FILTER_FILE") | grep -v -f <(sed -n -e 's/\s*#.*$//' -e '/^.\+$/p' "$SKIP_FILE") | sort
