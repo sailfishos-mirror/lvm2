0b147bf4fa7c5e99776b8d6667cec829cd9d58e4
Zdenek Kabelac <zkabelac@redhat.com>
2026-05-18 15:49:12 +0200

    test: update vdo convert
    Try running LV conversion with newer kernels.
    
    Conversion of Non-LV VDO volumes is also possible however for
    this is needed modified vdoprepareforlvm binary with fixes
    to avoid creation of unusable vdo metadata with v9 target version.

--- test/shell/vdo-convert.sh
+++ test/shell/vdo-convert.sh
@@ -37,7 +37,7 @@ if not which vdo ; then
 	# enable expansion of aliases within script itself
 	shopt -s expand_aliases
 	alias vdo='lvm_vdo_wrapper'
-	export VDO_BINARY=lvm_vdo_wrapper
+	export VDO_BINARY=$(command -v lvm_vdo_wrapper)
 	echo "Using 'lvm_vdo_wrapper' emulation of 'vdo' manager."
 fi
 which mkfs.ext4 || skip
@@ -45,9 +45,6 @@ export MKE2FS_CONFIG="$TESTDIR/lib/mke2fs.conf"
 
 # Conversion can be made with this version of vdo driver
 aux have_vdo 6 2 3 || skip
-
-# With new upstream VDO conversion is not supported
-aux have_vdo 9 0 0 && skip
 
 aux prepare_devs 2 20000
 
@@ -73,8 +70,14 @@ mkfs -E nodiscard "$DM_DEV_DIR/mapper/$VDONAME"
 # Different VG name fails
 not lvm_import_vdo -y -v --name $vg1/$lv1 "$DM_DEV_DIR/$vg/$lv1"
 
-# Try just dry run and observe logging
-lvm_import_vdo --dry-run -y -v --name $lv1 "$DM_DEV_DIR/$vg/$lv1"
+# Dry-run previews are visible on stderr without enabling verbose mode.
+cp "$VDO_CONFIG" vdoconf.before
+lvm_import_vdo --dry-run -y --name $lv1 "$DM_DEV_DIR/$vg/$lv1" >out 2>err
+grep 'Dry execution:' err
+not grep 'Dry execution:' out
+not grep 'Executing ' err
+cmp "$VDO_CONFIG" vdoconf.before
+check lv_exists $vg $lv1
 
 lvm_import_vdo -y --name $lv1 "$DM_DEV_DIR/$vg/$lv1"
 
@@ -98,6 +101,9 @@ vgremove -f $vg
 
 # ensure VDO device is not left in config file
 vdo remove $VDOCONF --force --name "$VDONAME" 2>/dev/null || true
+
+# Non-LV conversion requires modified vdoprepareforlvm compatible with kernel VDO version
+aux have_vdo 9 0 0 && exit 0
 
 aux wipefs_a "$dev1"
 
@@ -197,8 +203,8 @@ vdo create $VDOCONF --name "$VDONAME" --device "$TEST" --vdoSlabSize 128M --vdoL
 dmsetup table
 
 # Get VDO table line
-dmsetup table "$VDONAME" | tr " " "\n" | sed -e '5,6d' -e '12d' | tee vdo-orig
-
+# strip device(5,6), dedup flag(10), name(12) for stable comparison
+dmsetup table "$VDONAME" | tr " " "\n" | sed -e '5,6d' -e '10d' -e '12d' | tee vdo-orig
 mkfs.ext4 -E nodiscard "$DM_DEV_DIR/mapper/$VDONAME"
 rm -f debug.log*
 
@@ -230,7 +236,7 @@ dmsetup table
 fsck -n "$DM_DEV_DIR/$vg/$lv"
 
 # Compare converted LV uses same VDO table line
-dmsetup table "$vg-${lv}_vpool-vpool" | tr " " "\n" | sed -e '5,6d' -e '12d' | tee new-vdo-lv
+dmsetup table "$vg-${lv}_vpool-vpool" | tr " " "\n" | sed -e '5,6d' -e '10d' -e '12d' | tee new-vdo-lv
 
 tail -n+3 vdo-orig >vdo-orig-3
 tail -n+3 new-vdo-lv >new-vdo-lv-3
