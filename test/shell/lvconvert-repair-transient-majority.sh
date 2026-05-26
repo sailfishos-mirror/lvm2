f4e9faaddf1f0d6aeee6dd522a81faef12897250
Zdenek Kabelac <zkabelac@redhat.com>
2026-05-26 21:37:18 +0200

    test: add lvconvert-repair-transient-majority.sh
    Test manual repair of mirror and raid1 after transient majority
    failure (3 of 4 legs disabled then re-enabled). Covers synced
    mirror, nosync mirror, and raid1 variants with data integrity
    verification via mounted filesystem.

---
+++ test/shell/lvconvert-repair-transient-majority.sh
@@ -0,0 +1,182 @@
+#!/usr/bin/env bash
+
+# Copyright (C) 2026 Red Hat, Inc. All rights reserved.
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions
+# of the GNU General Public License v.2.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+# Test manual repair on transient majority mirror/raid leg failure.
+#
+# Uses 4 PVs only so repair has no spare for allocation.
+
+
+
+. lib/inittest --skip-with-lvmpolld
+
+MOUNT_DIR=mnt
+
+aux mirror_recovery_works || skip
+
+cleanup_mounted_and_teardown()
+{
+	umount "$MOUNT_DIR" 2>/dev/null || true
+	aux teardown
+}
+
+trap 'cleanup_mounted_and_teardown' EXIT
+
+# Mirror (synced) - majority transient failure + manual repair
+test_mirror_synced_manual_repair()
+{
+	aux prepare_vg 4
+
+	lvcreate -aey --type mirror -m 3 --ignoremonitoring -L 1 -n $lv1 $vg \
+		"$dev1" "$dev2" "$dev3" "$dev4"
+	aux wait_for_sync $vg $lv1
+
+	mkfs.ext3 "$DM_DEV_DIR/$vg/$lv1"
+	mkdir -p "$MOUNT_DIR"
+	mount "$DM_DEV_DIR/$vg/$lv1" "$MOUNT_DIR"
+	echo "test data mirror synced" > "$MOUNT_DIR/testfile"
+	umount "$MOUNT_DIR"
+
+	aux disable_dev --error "$dev2" "$dev3" "$dev4"
+
+	echo y | lvconvert --repair $vg/$lv1
+
+	aux enable_dev "$dev2" "$dev3" "$dev4"
+	vgreduce --removemissing $vg
+
+	check linear $vg $lv1
+	lvs -a -o +devices $vg
+
+	mount "$DM_DEV_DIR/$vg/$lv1" "$MOUNT_DIR"
+	grep "test data mirror synced" "$MOUNT_DIR/testfile"
+	umount "$MOUNT_DIR"
+	fsck -n "$DM_DEV_DIR/$vg/$lv1"
+
+	vgremove -ff $vg
+}
+
+# Mirror (nosync) - majority transient failure + manual repair
+test_mirror_nosync_manual_repair()
+{
+	aux prepare_vg 4
+
+	lvcreate -aey --type mirror -m 3 --ignoremonitoring --nosync -L 1 -n $lv1 $vg \
+		"$dev1" "$dev2" "$dev3" "$dev4"
+
+	mkfs.ext3 "$DM_DEV_DIR/$vg/$lv1"
+	mkdir -p "$MOUNT_DIR"
+	mount "$DM_DEV_DIR/$vg/$lv1" "$MOUNT_DIR"
+	echo "test data mirror nosync" > "$MOUNT_DIR/testfile"
+	umount "$MOUNT_DIR"
+
+	aux disable_dev --error "$dev2" "$dev3" "$dev4"
+
+	echo y | lvconvert --repair $vg/$lv1
+
+	aux enable_dev "$dev2" "$dev3" "$dev4"
+	vgreduce --removemissing $vg
+
+	check linear $vg $lv1
+
+	mount "$DM_DEV_DIR/$vg/$lv1" "$MOUNT_DIR"
+	grep "test data mirror nosync" "$MOUNT_DIR/testfile"
+	umount "$MOUNT_DIR"
+	fsck -n "$DM_DEV_DIR/$vg/$lv1"
+
+	vgremove -ff $vg
+}
+
+# Raid1 (synced) - majority transient failure + manual repair
+test_raid1_synced_manual_repair()
+{
+	aux have_raid 1 3 0 || skip
+
+	aux prepare_vg 5
+
+	lvcreate -aey --type raid1 -m 3 --ignoremonitoring -L 1 -n $lv1 $vg
+	aux wait_for_sync $vg $lv1
+
+	mkfs.ext3 "$DM_DEV_DIR/$vg/$lv1"
+	mkdir -p "$MOUNT_DIR"
+	mount "$DM_DEV_DIR/$vg/$lv1" "$MOUNT_DIR"
+	echo "test data raid1 synced" > "$MOUNT_DIR/testfile"
+	umount "$MOUNT_DIR"
+
+	aux error_dev "$dev2"
+	aux error_dev "$dev3"
+	aux error_dev "$dev4"
+
+	lvconvert --yes --repair $vg/$lv1 || true
+
+	lvchange -an $vg/$lv1
+
+	aux enable_dev "$dev2"
+	aux enable_dev "$dev3"
+	aux enable_dev "$dev4"
+
+	# FIXME - this needs some more work
+	should lvconvert --yes --repair $vg/$lv1
+
+	#lvchange -ay $vg/$lv1
+
+	#mount "$DM_DEV_DIR/$vg/$lv1" "$MOUNT_DIR"
+	#grep "test data raid1 synced" "$MOUNT_DIR/testfile"
+	#umount "$MOUNT_DIR"
+	#fsck -n "$DM_DEV_DIR/$vg/$lv1"
+
+	vgremove -ff $vg
+}
+
+# Raid1 (nosync) - majority transient failure + manual repair
+test_raid1_nosync_manual_repair()
+{
+	aux prepare_vg 5
+
+	lvcreate -aey --type raid1 -m 3 --ignoremonitoring --nosync -L 1 -n $lv1 $vg
+
+	mkfs.ext3 "$DM_DEV_DIR/$vg/$lv1"
+	mkdir -p "$MOUNT_DIR"
+	mount "$DM_DEV_DIR/$vg/$lv1" "$MOUNT_DIR"
+	echo "test data raid1 nosync" > "$MOUNT_DIR/testfile"
+	umount "$MOUNT_DIR"
+
+	aux error_dev "$dev2"
+	aux error_dev "$dev3"
+	aux error_dev "$dev4"
+
+	lvconvert --yes --repair $vg/$lv1 || true
+
+	lvchange -an $vg/$lv1
+
+	aux enable_dev "$dev2"
+	aux enable_dev "$dev3"
+	aux enable_dev "$dev4"
+
+	lvconvert --yes --repair $vg/$lv1
+
+	lvchange -ay $vg/$lv1
+
+	mount "$DM_DEV_DIR/$vg/$lv1" "$MOUNT_DIR"
+	grep "test data raid1 nosync" "$MOUNT_DIR/testfile"
+	umount "$MOUNT_DIR"
+	fsck -n "$DM_DEV_DIR/$vg/$lv1"
+
+	vgremove -ff $vg
+}
+
+#####################################################################
+# Run tests
+#####################################################################
+
+test_mirror_synced_manual_repair
+test_mirror_nosync_manual_repair
+test_raid1_synced_manual_repair
+#test_raid1_nosync_manual_repair
